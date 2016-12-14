5f3e36cf6b2620af41e304552012f2e34318a6e5
Paul Bartholomew <ptb0890@gmail.com>
2016-12-14 22:49:01 +0000

    Adding some additional packages

--- configs/Cask_emacs.d
+++ configs/Cask_emacs.d
@@ -1,4 +1,9 @@
 (source melpa)
 (source gnu)
 
+(depends-on "anaconda-mode")
+(depends-on "company")
+(depends-on "company-anaconda")
+(depends-on "company-auctex")
+(depends-on "flycheck")
 (depends-on "pallet")
