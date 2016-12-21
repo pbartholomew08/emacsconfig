3661abae788254b3a3bfbdc8b1e011a2bddd0cad
Paul Bartholomew <ptb0890@gmail.com>
2016-12-21 13:28:27 +0000

    Python config - using anaconda-mode

---
+++ configs/my_python.el
@@ -0,0 +1,56 @@
+;;; my_python.el --- python config file.
+;; 
+;; Filename: my_python.el
+;; Description: Configuration file for editing python.
+;; Author: Paul Bartholomew
+;; Maintainer: Paul Bartholomew
+;; Created: Thu Dec 15 22:19:50 2016 (+0000)
+;; Version: 0.0
+;; Package-Requires: (anaconda-mode)
+;; Last-Updated: Thu Dec 15 22:23:34 2016 (+0000)
+;;           By: Paul Bartholomew
+;;     Update #: 1
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary:
+;; 
+;;  This uses anaconda-mode (which is also used by spacemacs for
+;;  example).
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode) ; enables showing documentation using eldoc
+(eval-after-load "company"
+  '(add-to-list 'company-backends 'company-anaconda))
+
+(provide 'my_python)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; my_python.el ends here
