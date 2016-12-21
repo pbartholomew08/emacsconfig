38654f4f74829e07e9a1f247974e910ff6d05ad1
Paul Bartholomew <ptb0890@gmail.com>
2016-12-21 13:27:52 +0000

    Porting keyboard bindings

---
+++ configs/my_kbd.el
@@ -0,0 +1,64 @@
+;;; my_kbd.el --- keyboard bindings.
+;; 
+;; Filename: my_kbd.el
+;; Description: Config file for keyboards bindings.
+;; Author: Paul Bartholomew
+;; Maintainer:
+;; Created: Fri Dec 16 23:40:37 2016 (+0000)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated: Fri Dec 16 23:42:59 2016 (+0000)
+;;           By: Paul Bartholomew
+;;     Update #: 4
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary:
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+(defun buildScript ()
+  "Run the buildscript .buildScript.sh."
+  (interactive)
+  (shell-command "./.buildScript.sh")
+  )
+(global-set-key (kbd "C-c b") 'buildScript)
+
+; Buffer cycling
+(global-set-key (kbd "C-S-<prior>") 'previous-buffer)
+(global-set-key (kbd "C-S-<next>") 'next-buffer)
+
+; Close buffer
+(global-set-key (kbd "C-S-w") 'delete-window)
+
+(provide 'my_kbd)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; my_kbd.el ends here
