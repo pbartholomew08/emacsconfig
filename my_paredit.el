989ab10e6c5131207863ecc845b07a5fc0b6c571
Paul <ptb0890@gmail.com>
2021-02-16 12:55:12 +0000

    Improve paredit keybindings
    When slurping/barfing sexps it makes more sense (to me) to use C-<left/right> to
    do the backward (i.e. left) operations and C-M-<left/right> to do the forward
    (i.e. right) operations

--- my_paredit.el
+++ my_paredit.el
@@ -4,15 +4,30 @@
 ;;
 ;;; Code:
 
+(require 'paredit)
+
 (autoload 'enable-paredit-mode
   "paredit"
   "Turn on pseudo-structural editing of Lisp code."
   t)
 
+;;; Keybindings
+
+;; The slurp and bar commands feel back to front
+(add-hook 'paredit-mode-hook
+	 (lambda ()
+	   (local-set-key (kbd "C-<left>") 'paredit-backward-slurp-sexp)
+	   (local-set-key (kbd "C-<right>") 'paredit-backward-barf-sexp)
+	   (local-set-key (kbd "C-M-<left>") 'paredit-forward-barf-sexp)
+	   (local-set-key (kbd "C-M-<right>") 'paredit-forward-slurp-sexp)))
+
+;; Fix keybindings overridden by evil
+(require 'evil)
 (add-hook 'paredit-mode-hook
 	  (lambda ()
 	    (evil-local-set-key 'insert (kbd "C-k") 'paredit-kill)))
 
+;;; Enable paredit in lisp modes
 (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
